a11536fb0db34c30d712fa89b7418c14f6d3d5a2
bete <beteme390@gmail.com>
2024-04-27 05:31:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       <h1>bete thegreat</h1>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
